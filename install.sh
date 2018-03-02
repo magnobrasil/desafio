0b7f3620cb4b2cdf754d257a0e7cdf2134a7ce6c
magnobrasil <36795262+magnobrasil@users.noreply.github.com>
2018-03-01 21:53:45 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,16 @@
 yum update -y
 yum install  git curl wget unzip zip python27  -y
 
-curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-
-python get-pip.py
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py" && python get-pip.py
 
 
-# Baixando e descompactando arquivos necessários no GIT
+# Ambiente Gráfico
+yum groupinstall "X Window System" && yum groupinstall "Fonts" && yum install kde-workspace && yum install gdm && unlink /etc/systemd/system/default.target && ln -sf /lib/systemd/system/graphical.target /etc/systemd/system/default.target
+
+reboot -n
+
+
+#Baixando e descompactando arquivos do GIT
 
 mkdir /opt/projeto
 cd /opt/projeto
@@ -18,6 +22,4 @@ cd desafio-master
 
 chmod +x install_full.sh
 
-/opt/projeto/desafio-master/install_full.sh
-
-
+/opt/projeto/desafio-master/install_full.sh" > install.sh
